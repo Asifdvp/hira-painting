db7b8aba38edf831129ea8ad1923e321dbf1a03a
amustafayev <amustafayev@rth.gov.az>
2025-07-10 21:06:06 +0400

    updates

--- src/js/order.js
+++ src/js/order.js
@@ -1,9 +1,22 @@
 flatpickr("#date", {
-        dateFormat: "Y-m-d",
-      });
-      flatpickr("#customTime", {
-        enableTime: true,
-        noCalendar: true,
-        dateFormat: "H:i",
-        time_24hr: true,
-      });
+  dateFormat: "Y-m-d",
+});
+flatpickr("#customTime", {
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i",
+  time_24hr: true,
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const selectedService = urlParams.get("service");
+console.log(urlParams.get("service"));
+if (selectedService) {
+  const selectEl = document.getElementById("service-select");
+  for (let option of selectEl.options) {
+    if (option.value === selectedService) {
+      option.selected = true;
+      break;
+    }
+  }
+}
