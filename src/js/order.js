54c2e03a47920ac09b52b5a4bc54915ec46ad8a4
Asifdvp <asif.mustafayev.13@gmail.con>
2025-07-13 09:40:45 +0400

    font

--- src/js/order.js
+++ src/js/order.js
@@ -1,12 +1,4 @@
-flatpickr("#date", {
-  dateFormat: "Y-m-d",
-});
-flatpickr("#customTime", {
-  enableTime: true,
-  noCalendar: true,
-  dateFormat: "H:i",
-  time_24hr: true,
-});
+
 
 const urlParams = new URLSearchParams(window.location.search);
 const selectedService = urlParams.get("service");
